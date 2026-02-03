41085208c52af4c784d774e225dd78f724fd9e8f
yeasinrahman26 <yeasinrahmansafa@gmail.com>
2026-02-03 06:30:45 +0600

    fix catch-all route

--- Chatapp_backend/src/index.js
+++ Chatapp_backend/src/index.js
@@ -29,7 +29,7 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "Chatapp_frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(
       path.join(__dirname, "Chatapp_frontend/dist", "index.html"),
     );
